c4317abf1c67c6fce7be6c1e473ba7021d1b0aa4
Kayode96max <olukayodeusman406@gmail.com>
2025-11-21 06:43:30 +0100

    copyright

--- frontend/src/components/Discount.jsx
+++ frontend/src/components/Discount.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Discount() {
   return (
-    <div className='mt-4'>
-      <div className='relative bg-black text-white text-xs text-center px-4 py-2 font-sans h-8 flex items-center justify-center'>
+    <div>
+      <div className='relative bg-black text-white text-xs text-center px-4 py-2 font-sans flex items-center justify-center'>
         <p>Summer Sales for All Swim Suits And Free Express Delivery - 50% OFF!<span className='text-sm font-bold'>  Shop Now</span></p>
         <div className="language absolute right-32 flex items-center">
           <select className='bg-black text-white text-xs outline-none  border-0 m-0 rounded-2xl'>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,8 +4,9 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div>
-        <footer className='bg-black flex justify-evenly flex-row p-4 text-sm text-white font-sans'>
-            <div className="flex flex-col gap-2">
+        <footer className='bg-black flex flex-col p-4 text-sm text-white font-sans'>
+            <div className='flex justify-evenly flex-row'>
+                <div className="flex flex-col gap-2">
                 <p className='font-bold text-white text-base' >EXCLUSIVE</p>
                 <p>Subscribe</p>
                 <p>Get 10% off your first order</p>
@@ -15,48 +16,51 @@ const Footer = () => {
                     type="email" 
                     placeholder='Enter your email' 
                     className='pl-2 pr-4 py-2 bg-black text-white font-bold placeholder-gray-400 border-white border outline-none rounded'
-                    />
-                </div>
-            </div>
-            <div className="flex flex-col gap-2">
-                <p className='font-bold text-white text-base' >SUPPORT</p>
-                <p>111, Bijoy Sarani,<br />DH 1515, Bangladesh</p>
-                <p>exclusive@gmail.com</p>
-                <p>+88015-88888-9999</p>
-            </div>
-            <div className="flex flex-col gap-2">
-                <p className='font-bold text-white text-base' >Account</p>
-                <p>My Account</p>
-                <p>Login/Register</p>
-                <p>Cart</p>
-                <p>Wishlist</p>
-                <p>Shop</p>
-            </div>
-            <div className="flex flex-col gap-2">
-                <p className='font-bold text-white text-base' >Quick Links</p>
-                <p >Privacy Policy</p>
-                <p>Terms Of Use</p>
-                <p>FAQ</p>
-                <p>Contact</p>
-                <p>About Us</p>
-            </div>
-            <div className="flex flex-col gap-2">
-                <p className='font-bold text-white text-base' >Download App</p>
-                <p className='text-gray-300'>Save $3 with App - New Users Only</p>
-                <div className="flex flex-row gap-2 items-center justify-center">
-                    <img src={assets.qrcode} alt="qr code" className='' />
-                    <div className="flex flex-col gap-3 ">
-                        <img src={assets.playstore} alt="google-play-badge" className='w-30 cursor-pointer' />
-                        <img src={assets.app_store} alt="app-store-badge" className='w-30 cursor-pointer' />
+                        />
                     </div>
                 </div>
-                <div className="flex flex-row gap-4 mt-4 justify-evenly">
-                    <img src={assets.facebook} alt="facebook" className='w-6 h-6 cursor-pointer' />
-                    <img src={assets.twitter} alt="twitter" className='w-6 h-6 cursor-pointer' />
-                    <img src={assets.instagram} alt="instagram" className='w-6 h-6 cursor-pointer' />
-                    <img src={assets.linkedin} alt="linkedin" className='w-6 h-6 cursor-pointer' />
+                <div className="flex flex-col gap-2">
+                    <p className='font-bold text-white text-base' >SUPPORT</p>
+                    <p>111, Bijoy Sarani,<br />DH 1515, Bangladesh</p>
+                    <p>exclusive@gmail.com</p>
+                    <p>+88015-88888-9999</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p className='font-bold text-white text-base' >Account</p>
+                    <p>My Account</p>
+                    <p>Login/Register</p>
+                    <p>Cart</p>
+                    <p>Wishlist</p>
+                    <p>Shop</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p className='font-bold text-white text-base' >Quick Links</p>
+                    <p >Privacy Policy</p>
+                    <p>Terms Of Use</p>
+                    <p>FAQ</p>
+                    <p>Contact</p>
+                    <p>About Us</p>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <p className='font-bold text-white text-base' >Download App</p>
+                    <p className='text-gray-300'>Save $3 with App - New Users Only</p>
+                    <div className="flex flex-row gap-2 items-center justify-center">
+                        <img src={assets.qrcode} alt="qr code" className='' />
+                        <div className="flex flex-col gap-3 ">
+                            <img src={assets.playstore} alt="google-play-badge" className='w-30 cursor-pointer' />
+                            <img src={assets.app_store} alt="app-store-badge" className='w-30 cursor-pointer' />
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-4 mt-4 justify-evenly">
+                        <img src={assets.facebook} alt="facebook" className='w-6 h-6 cursor-pointer' />
+                        <img src={assets.twitter} alt="twitter" className='w-6 h-6 cursor-pointer' />
+                        <img src={assets.instagram} alt="instagram" className='w-6 h-6 cursor-pointer' />
+                        <img src={assets.linkedin} alt="linkedin" className='w-6 h-6 cursor-pointer' />
+                    </div>
                 </div>
             </div>
+            <hr className="border-t border-gray-500 mt-2" />
+            <p className="text-center text-gray-300 text-xs mt-2">© 2025 Kayode96Max. All rights reserved.</p>
         </footer>
     </div>
   )
